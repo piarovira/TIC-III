fa5d82635737b2a0eb945c1b67c9dce0068a7fa3
Pia Rovira <piarovira98@gmail.com>
2019-10-14 20:58:45 -0300

    arreglo flechitas | igua hay qeu ver como hacer si se cierran sin hacer click

--- js/script.js
+++ js/script.js
@@ -4,10 +4,43 @@ $(document).ready(function () {
         $('.animated-icon1').toggleClass('open');
     });
 
-    $(".card-header").click(function () {
-        $(".rotate").toggleClass("down");
+    $("#card1").click(function () {
+        $(".rotate1").toggleClass("down");
     })
-
+    $("#card2").click(function () {
+        $(".rotate2").toggleClass("down");
+    })
+    $("#card3").click(function () {
+        $(".rotate3").toggleClass("down");
+    })
+    $("#card4").click(function () {
+        $(".rotate4").toggleClass("down");
+    })
+    $("#card5").click(function () {
+        $(".rotate5").toggleClass("down");
+    })
+    $("#card6").click(function () {
+        $(".rotate6").toggleClass("down");
+    })
+    $("#card7").click(function () {
+        $(".rotate7").toggleClass("down");
+    })
+    $("#card8").click(function () {
+        $(".rotate8").toggleClass("down");
+    })
+    $("#card9").click(function () {
+        $(".rotate9").toggleClass("down");
+    })
+    $("#card10").click(function () {
+        $(".rotate10").toggleClass("down");
+    })
+    $("#card11").click(function () {
+        $(".rotate11").toggleClass("down");
+    })
+    $("#card12").click(function () {
+        $(".rotate12").toggleClass("down");
+    })
+    
 });
 
 
